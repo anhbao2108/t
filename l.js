c0983d41aae21f94cdf09729314ec71e0e8c9dc0
anhbao2108 <n23dcpt003@student.ptithcm.edu.vn>
2025-12-10 19:42:24 +0700

    Create l.js

---
+++ l.js
@@ -0,0 +1,6 @@
+function showLove() {
+  const loveText = document.getElementById("loveText");
+
+  loveText.innerHTML = "Minh thương bạn nhiều lắm ❤️<br>Ở lại bên Banh lâu lâu nha 🥺";
+  loveText.classList.remove("hidden");
+}
